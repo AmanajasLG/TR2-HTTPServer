a5196375df1e143785006e7fed091034063d0ed8
AmanajasLG <luizamanajas@hotmail.com>
2018-12-02 23:02:19 -0200

    Muda nome do executavel e número de argumentos

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
 #include "proxy.hpp"
 #include <QApplication>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Proxy proxy;
+
+    if (argc == 1)
+    {
+        proxy.SetPort();
+    }
+    else if (argc == 3)
+    {
+        if (!boost::iequals(argv[1], "-p"))
+        {
+            cout << "Argumento " << argv[1] << " invalido! " << endl;
+            cout << "Execucao deve ter formato ./aracne OU ./aracne -p <numero da porta>" << endl;
+            return 0;
+        }
+        proxy.SetPort(stoi(argv[2]));
+    }
+    else
+    {
+        cout << "Numero invalido de argumentos! " << endl;
+        cout << "Execucao deve ter formato ./aracne OU ./aracne -p <numero da porta>" << endl;
+        return 0;
+    }
+
+    proxy.InitThreads();
     proxy.show();
 
     return a.exec();
